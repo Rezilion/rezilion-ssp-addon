e1fd27b821dd3270a4ff63a05c8f789f5fa56a2d
LZL <liorz@rezilion.com>
2022-01-23 11:00:26 +0200

    Recreate addon

--- bin/main.ts
+++
@@ -1,9 +0,0 @@
-import { App } from '@aws-cdk/core';
-import * as ssp from '@aws-quickstart/ssp-amazon-eks';
-import { RezilionAddOn } from '../lib';
-
-const app = new App();
-
-ssp.EksBlueprint.builder()
-    .addOns(new RezilionAddOn('dynamic_test_api_key'))
-    .build(app, 'lzl-rezilion-addon');
